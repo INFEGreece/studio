c4076fc240e1d5ef588bff67026ef3f126d3ada2
INFE GREECE Official <infegreeceofficial@gmail.com>
2026-03-17 20:38:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2020467600-3cc22",
+  "appId": "1:956752732572:web:922bd0d69fed25539ba859",
+  "apiKey": "AIzaSyC5lDjpLmHKBaF8zQlEaSnPMoQubhweIl0",
+  "authDomain": "studio-2020467600-3cc22.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "956752732572"
+};
